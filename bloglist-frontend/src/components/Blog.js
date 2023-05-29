3944680b02987a82c0783b23e987cfacc82ba6eb
Hector Antonio Peñaloza Rivera <hector.penaloza@netlogistik.com>
2023-05-29 15:27:20 -0600

    bloglist-frontend 5.7

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,41 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({blog}) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [visible, setVisible] = useState(true)
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const simpleView = () => (
+    <>
+      <p>{blog.title} {blog.author} <button onClick={toggleVisibility}>view</button></p>
+    </>  
+  )
+
+  const detailedView = () => (
+    <>
+      <p>{blog.title} {blog.author} <button onClick={toggleVisibility}>hide</button></p>
+      <p>{blog.url}</p>
+      <p>likes {blog.likes} <button>like</button></p>
+    </>
+  )
+
+  return (
+    <div style={blogStyle}>
+      {visible ?
+      simpleView() :
+      detailedView()}
+    </div>
+  )
+}
 
 export default Blog
